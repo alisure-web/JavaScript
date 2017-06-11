67a18ca435c4873e7f0cb6b8539ff265c8961e0b
ALISURE <562282219@qq.com>
2017-06-11 19:44:45 +0800

    解构赋值（部分）

---
+++ src2/es6/demo/destructuring-assignment.js
@@ -0,0 +1,22 @@
+
+/* 参数解构 */
+/**
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+ */
+
+var o = {p: 42, q: true};
+var {p, q} = o; /* 解构赋值 */
+console.log(p); // 42
+console.log(q); // true
+
+/* 正常的 */
+function test(o) {
+    console.log(o.p);
+}
+test(o);
+
+/* 参数解构 */
+function test2({p}) {
+    console.log(p);
+}
+test(o);
